2636b51356bccd8836064c140ff4c6107f06f96e
tusharv2005 <tvarshney585@gmail.com>
2026-05-26 22:28:45 +0530

    Remove glow effect from loading screen star

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -24,7 +24,7 @@ export function LoadingScreen() {
     const timer = setTimeout(() => {
       setShow(false)
       document.documentElement.style.visibility = "visible"
-    }, 500)
+    }, 2000)
 
     return () => {
       clearTimeout(timer)
@@ -36,40 +36,19 @@ export function LoadingScreen() {
 
   return (
     <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black">
-      {/* 4-pointed star with blur/glow effect */}
-      <div className="relative flex items-center justify-center">
-        {/* Blurred background star for glow effect */}
-        <div
-          className="animate-spin-slow absolute h-64 w-64"
-          style={{ filter: "blur(20px)" }}
+      {/* 4-pointed star without glow */}
+      <div className="animate-spin-slow relative h-48 w-48">
+        <svg
+          viewBox="0 0 100 100"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-full w-full"
         >
-          <svg
-            viewBox="0 0 100 100"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-full w-full"
-          >
-            <path
-              d="M50 10 C 45 35, 35 45, 10 50 C 35 55, 45 65, 50 90 C 55 65, 65 55, 90 50 C 65 45, 55 35, 50 10 Z"
-              fill="white"
-            />
-          </svg>
-        </div>
-
-        {/* Sharp star on top */}
-        <div className="animate-spin-slow relative h-48 w-48">
-          <svg
-            viewBox="0 0 100 100"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-full w-full"
-          >
-            <path
-              d="M50 10 C 45 35, 35 45, 10 50 C 35 55, 45 65, 50 90 C 55 65, 65 55, 90 50 C 65 45, 55 35, 50 10 Z"
-              fill="white"
-            />
-          </svg>
-        </div>
+          <path
+            d="M50 10 C 45 35, 35 45, 10 50 C 35 55, 45 65, 50 90 C 55 65, 65 55, 90 50 C 65 45, 55 35, 50 10 Z"
+            fill="white"
+          />
+        </svg>
       </div>
     </div>
   )
